d2021f74037e87a6cb3b0dbc610aebface22179a
Daniel Suo <danielsuo@gmail.com>
2020-07-20 20:13:37 +0000

    in a second

--- vent/controller/control_module.py
+++ vent/controller/control_module.py
@@ -617,13 +617,13 @@ class ControlModuleBase:
         self.__control_signal_in = 0 #self._adaptivecontroller.feed(self._DATA_PRESSURE, now)
 
         if cycle_phase < self.__SET_I_PHASE:            
-            self.__control_signal_out = 1                                                        # close out valve
+            self.__control_signal_out = 0                                                        # close out valve
 
         elif cycle_phase < self.__SET_PEEP_TIME + self.__SET_I_PHASE:                                     # then, we drop pressure to PEEP
-            self.__control_signal_out = 1
+            self.__control_signal_out = 0
 
         elif cycle_phase < self.__SET_CYCLE_DURATION:
-            self.__control_signal_out = 1
+            self.__control_signal_out = 0
 
         else:
             self._cycle_start = time.time()  # New cycle starts
